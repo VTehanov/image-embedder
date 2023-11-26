43c4c0836aa523765cf66df5d5beaadded6bcbcd
Vasil Tehanov <vasiltehanov@gmail.com>
2023-11-26 12:53:10 +0200

    render mock image grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,34 @@
 
 import { UploadButton } from "~/components/uploadthing";
 
+const ImageGrid = () => {
+  const image = (
+    <div
+      className="h-48 w-48 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1700740760502-f28b1769c8d3?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+      }}
+    />
+  );
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {image}
+      {image}
+      {image}
+      {image}
+      {image}
+      {image}
+      {image}
+      {image}
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-8 p-24">
       <UploadButton
         endpoint="sampleImage"
         onClientUploadComplete={(res) => {
@@ -17,6 +42,8 @@ export default function Home() {
           alert(`ERROR! ${error.message}`);
         }}
       />
+
+      <ImageGrid />
     </main>
   );
 }
